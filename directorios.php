3551f58856cd1397b61251f58b939cdb33ff078c
Kike Algarra <kike@areavisual.net>
2023-05-11 10:55:32 +0200

    Ver directorio actual y padre
    Me permite ver el directorio actual y padre donde está ubicado.

---
+++ directorios.php
@@ -0,0 +1,7 @@
+<?php
+$directorio_actual = __DIR__; // Ruta del directorio actual
+$directorio_padre = dirname($directorio_actual); // Ruta del directorio padre
+
+echo "Directorio actual: $directorio_actual <br>";
+echo "Directorio padre: $directorio_padre <br>";
+?>
